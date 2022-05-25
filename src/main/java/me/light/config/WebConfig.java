870b2b64ac5ab191d1b7c7ca7e5cf107eaabe58b
cmskrns <cmskrns1@naver.com>
2022-05-25 12:32:59 +0900

    자바설정

---
+++ src/main/java/me/light/config/WebConfig.java
@@ -0,0 +1,33 @@
+package me.light.config;
+
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {ServletConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter efilter = new CharacterEncodingFilter();
+		efilter.setEncoding("utf-8");
+		efilter.setForceEncoding(true);
+		return new Filter[] {efilter};
+	}
+
+}
